074b4b9a133b48bcb6657ad4ce038bc73467eade
DracoSolim <gamergabo898@gmail.com>
2023-09-25 00:01:22 -0600

    Agregar archivos de proyecto.

---
+++ Blackjack/blackjack.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Blackjack
+{
+    partial class blackjack
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(blackjack));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 347);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Jugador 1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(387, 347);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Jugador 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(638, 347);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Jugador 3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(615, 391);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(364, 391);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(119, 391);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(149, 369);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Puntos";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(394, 369);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Puntos";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(645, 369);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Puntos";
+            // 
+            // blackjack
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(834, 561);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "blackjack";
+            this.Text = "BlackJack";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ Blackjack/master.cs
@@ -0,0 +1,80 @@
+﻿namespace Blackjack
+{
+    internal class master
+    {
+        class carta
+        {
+            public string id;
+            public string tipo;
+            public int valor;
+
+            static void Main(string[] args)
+            {
+                carta pica1 = new carta();
+                pica1.id = "p1";
+                pica1.tipo = "pica";
+                pica1.valor = 1;
+
+                carta pica2 = new carta();
+                pica2.id = "p2";
+                pica2.tipo = "pica";
+                pica2.valor = 2;
+
+                carta pica3 = new carta();
+                pica1.id = "p3";
+                pica1.tipo = "pica";
+                pica1.valor = 3;
+
+                carta pica4 = new carta();
+                pica1.id = "p4";
+                pica1.tipo = "pica";
+                pica1.valor = 4;
+
+                carta pica5 = new carta();
+                pica1.id = "p5";
+                pica1.tipo = "pica";
+                pica1.valor = 5;
+
+                carta pica6 = new carta();
+                pica1.id = "p6";
+                pica1.tipo = "pica";
+                pica1.valor = 6;
+
+                carta pica7 = new carta();
+                pica1.id = "p7";
+                pica1.tipo = "pica";
+                pica1.valor = 7;
+
+                carta pica8 = new carta();
+                pica1.id = "p8";
+                pica1.tipo = "pica";
+                pica1.valor = 8;
+
+                carta pica9 = new carta();
+                pica1.id = "p9";
+                pica1.tipo = "pica";
+                pica1.valor = 9;
+
+                carta pica10 = new carta();
+                pica1.id = "p10";
+                pica1.tipo = "pica";
+                pica1.valor = 10;
+
+                carta pica11 = new carta();
+                pica1.id = "p11";
+                pica1.tipo = "pica";
+                pica1.valor = 10;
+
+                carta pica12 = new carta();
+                pica1.id = "p12";
+                pica1.tipo = "pica";
+                pica1.valor = 10;
+
+                carta pica13 = new carta();
+                pica1.id = "p13";
+                pica1.tipo = "pica";
+                pica1.valor = 10;
+            }
+        }
+    }
+}
